31b183b4b5452145fbf44ec09252f88072760a6d
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-10-10 01:28:08 +0300

    Change workers for reports build

--- app/workers/new_reports_worker.rb
+++ app/workers/new_reports_worker.rb
@@ -3,7 +3,32 @@ class NewReportsWorker
 
   sidekiq_options retry: true, failure: true
 
-  def perform(report, recipient_email, start_date, end_date)
+  def perform(start_date, end_date, recipient_email)
+    report = build_report(start_date, end_date)
+
     ReportsMailer.new_report_by_author(report, recipient_email, start_date, end_date).deliver
+  end
+
+  private
+
+  def build_report(start_date, end_date)
+    hash = {}
+
+    User.pluck(:id, :email, :nickname).each do |user|
+      posts_size = Post.where(author_id: user[0]).posts_for_period(start_date, end_date).size
+      comments_size = Comment.where(author_id: user[0]).comments_for_period(start_date, end_date).size
+
+      position = posts_size + comments_size / 10
+
+      hash[user[0]] = {}
+      hash[user[0]]['email'] = user[1]
+      hash[user[0]]['nickname'] = user[2]
+      hash[user[0]]['posts_size'] = posts_size
+      hash[user[0]]['comments_size'] = comments_size
+      hash[user[0]]['position'] = position
+    end
+
+    new_hash = hash.sort_by { |key, value| hash[key]['position'] }.reverse
+    new_hash.to_h
   end
 end
--- spec/workers/schedule_new_reports_worker_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ScheduleNewReportsWorker, type: :worker do
-  it 'should to check the start of the worker' do
-    ScheduleNewReportsWorker.perform_async(5.years.ago, 2.years.ago, 'example@example.co')
-    expect(ScheduleNewReportsWorker.jobs.size).to eq 1
-  end
-end
